28e76b4e88993b96bb3b99bf56240f23b60014ee
aminakhatun22 <aminak26802@gmail.com>
2022-09-15 00:26:36 +0600

    btn-active

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -109,22 +109,31 @@ const start = () => {
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = '<h1>${count}</h1>';
 
+
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
+
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
+      // display.remove("inactive");
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
+
+
     }
+
     count--;
   }, 1000);
+
 };
+
 
 // START Countdown
 startBtn.addEventListener("click", start);
+
 
 // If history exists, show it
 displayHistory();
